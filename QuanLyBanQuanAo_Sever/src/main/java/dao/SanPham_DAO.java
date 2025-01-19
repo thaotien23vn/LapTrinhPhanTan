5c78e92ed67eb14aa2c98e40cbe0f66770dc70d8
Thảo Tiên <143621516+thaotien23vn@users.noreply.github.com>
2025-01-19 12:47:58 +0700

    Add files via upload

---
+++ QuanLyBanQuanAo_Sever/src/main/java/dao/SanPham_DAO.java
@@ -0,0 +1,60 @@
+package dao;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+import dao_interface.SanPham_DAO_Interface;
+import entities.SanPham;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class SanPham_DAO extends UnicastRemoteObject implements SanPham_DAO_Interface {
+    
+	 private EntityManager entityManager;
+
+	    public SanPham_DAO() throws RemoteException {
+	        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mssql");
+	        entityManager = entityManagerFactory.createEntityManager();
+	    }
+
+    public boolean create(SanPham sanpham) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(sanpham);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean delete(String maSanPham) {
+        try {
+        	SanPham sanpham = entityManager.find(SanPham.class, maSanPham);
+            if (sanpham != null) {
+                entityManager.getTransaction().begin();
+                entityManager.remove(sanpham);
+                entityManager.getTransaction().commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+	@Override
+	public boolean update(SanPham sanpham) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
