26b5be2643bc157f01eeaa3cf152ee1f377ff4b5
Thảo Tiên <143621516+thaotien23vn@users.noreply.github.com>
2025-01-20 18:05:33 +0700

    Update SanPham_DAO_Interface.java

--- QuanLyBanQuanAo_Sever/src/main/java/dao_interface/SanPham_DAO_Interface.java
+++ QuanLyBanQuanAo_Sever/src/main/java/dao_interface/SanPham_DAO_Interface.java
@@ -3,6 +3,8 @@ package dao_interface;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+
+import entities.KhachHang;
 import entities.SanPham;
 
 public interface SanPham_DAO_Interface extends Remote {
@@ -12,5 +14,9 @@ public interface SanPham_DAO_Interface extends Remote {
     boolean update(SanPham sanpham) throws RemoteException;
     
     boolean delete(String maSanPham) throws RemoteException;
+    
+    boolean updateTrangThaiSanPham(SanPham sanpham) throws RemoteException;
+    
+    boolean updateKH(KhachHang kh) throws RemoteException;
     
 }
