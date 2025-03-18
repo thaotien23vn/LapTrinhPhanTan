9143b3da21987967aadf6915d82e8a8d76979781
Ncam26 <94227320+Ncam26@users.noreply.github.com>
2025-03-18 13:40:17 +0700

    entities minh

---
+++ serverAM/src/main/java/org/server/entities/OrderItem.java
@@ -0,0 +1,89 @@
+package org.server.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore // Tránh vòng lặp vô hạn khi serialize JSON
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private double price; // Lưu giá sản phẩm tại thời điểm đặt hàng
+
+    public OrderItem() {}
+
+    public OrderItem(Order order, Product product, int quantity, double price) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // Tính tổng giá trị của sản phẩm trong đơn hàng
+    public double getTotalPrice() {
+        return this.quantity * this.price;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItem [id=" + id + ", order=" + order + ", product=" + product + ", quantity=" + quantity
+				+ ", price=" + price + "]";
+	}
+
+    
+}
